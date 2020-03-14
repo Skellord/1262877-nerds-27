f4486cb7165a58c998caa62a4dbb708e01a0051a
Skellord <59919774+Skellord@users.noreply.github.com>
2020-03-14 20:35:45 +0300

    Правки 5.0

--- js/script.js
+++ js/script.js
@@ -34,11 +34,14 @@ link.addEventListener("click", function (evt) {
 close.addEventListener("click", function (evt) {
     evt.preventDefault();
     popup.classList.remove("modal-show");
+    popup.classList.remove("modal-error");
 })
 form.addEventListener ("submit", function (evt) {
     if (!login.value || !email.value || !text.value) {
     evt.preventDefault();
-    console.log("Необходимо заполнить все поля");
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error")
     } else {
         if (isStorageSupport){
         localStorage.setItem("login", login.value);
@@ -51,6 +54,7 @@ window.addEventListener("keydown", function (evt) {
         evt.preventDefault();
         if (popup.classList.contains("modal-show")) {
             popup.classList.remove("modal-show");
+            popup.classList.remove("modal-error");
         }
     }
 })
